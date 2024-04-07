810d49ca67a78c8e1f3e84e5d7c4220a8067771e
Rohith V <96191520+RohithV123@users.noreply.github.com>
2024-04-08 00:58:16 +0530

    Fourth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,11 @@ function App() {
 
   return (
      <div>
-<<<<<<< HEAD
+
       <h1>Wlcome.......</h1>
       <h2>Hellloooooo</h2>
-=======
+
       <h1>Welcome.......</h1>
->>>>>>> 79809fe22714fad02b01db6ed3e6927bb137eab4
      </div>
   )
 }
