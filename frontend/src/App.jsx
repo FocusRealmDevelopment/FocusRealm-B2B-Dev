837f8d4f21c32263b1f993472b54ae903d840193
pranulkbv28 <28bvpranaav@gmail.com>
2024-04-08 21:10:36 +0530

    frontend boiler plate added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-<<<<<<< HEAD
     <div>
       <div>
         <SidePanel />
@@ -16,17 +15,6 @@ function App() {
       </div>
     </div>
   );
-=======
-     <div>
-
-      <h1>Wlcome.......</h1>
-      <h2>Hellloooooo</h2>
-
-      <h1>Welcome.......</h1>
-      <h2>Soethingg</h2>
-     </div>
-  )
->>>>>>> 952b855a4bea623336b35567a8a4f54f5dbe6ad1
 }
 
 export default App;
