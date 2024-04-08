c76ca6712c8450945dd4bd0d3f9b4f2d9fa99c66
pranulkbv28 <28bvpranaav@gmail.com>
2024-04-09 02:34:55 +0530

    lightmode sidebar navbar completed

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,9 +1,45 @@
 // import React from 'react';
+import styles from "./navbar.module.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import profilePic from "../../assets/images/profilePic.png";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>This is the Navbar.</h1>
+    <div className={styles.body}>
+      <div className={styles.searchBarContainer}>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search all important requirement"
+        />
+        <button>
+          <SearchIcon />
+        </button>
+      </div>
+      <div className={styles.navIcons}>
+        <div className={styles.navIconContainer}>
+          <ChatBubbleIcon />
+        </div>
+        <div className={styles.navIconContainer}>
+          <CalendarTodayIcon />
+        </div>
+        <div className={styles.navIconContainer}>
+          <NotificationsIcon />
+        </div>
+      </div>
+      <div className={styles.profileInfoContainer}>
+        <div className={styles.profilePicContainer}>
+          <img src={profilePic} alt="profile-pic" />
+        </div>
+        <div className={styles.nameInfoContainer}>
+          <p className={styles.studentName}>Student name</p>
+          <p>Student Class</p>
+        </div>
+      </div>
     </div>
   );
 };
