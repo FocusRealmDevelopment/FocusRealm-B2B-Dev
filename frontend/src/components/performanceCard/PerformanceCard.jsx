a9d9af9e06c89d8308b120483fa4c740bbe1bb8a
pranulkbv28 <28bvpranaav@gmail.com>
2024-04-30 14:59:32 +0530

    profile page v2

--- frontend/src/components/performanceCard/PerformanceCard.jsx
+++ frontend/src/components/performanceCard/PerformanceCard.jsx
@@ -1,8 +1,39 @@
+/* eslint-disable react/prop-types */
 // import React from 'react';
+import { Link } from "react-router-dom";
 import styles from "./performancecard.module.css";
+import { MdOutlineInsertChart } from "react-icons/md";
 
-const PerformanceCard = () => {
-  return <div className={`${styles.body}`}>This is the Card.</div>;
+const PerformanceCard = ({
+  logo,
+  componentName,
+  componentDesc,
+  path,
+  logo2,
+  children,
+}) => {
+  return (
+    <div
+      className={`${styles.body} p-4 rounded-xl flex items-center justify-between`}
+    >
+      <div className={`flex gap-3`}>
+        {logo && <img src={logo} alt="logo-pic" />}
+        <div>
+          <h1 className={``}>{componentName}</h1>
+          <h1 className={``}>{componentDesc}</h1>
+          {children}
+        </div>
+      </div>
+      <div>
+        {path && (
+          <Link to={path}>
+            <MdOutlineInsertChart />
+          </Link>
+        )}
+        {logo2 && <img src={logo2} alt="second-logo-pic" />}
+      </div>
+    </div>
+  );
 };
 
 export default PerformanceCard;
